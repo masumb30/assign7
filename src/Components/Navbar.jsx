fad10d679e83150e3bfff89f27b8b2219d67d3c5
masumb30 <masum30.b@gmail.com>
2026-04-15 20:16:35 +0600

    stats page added

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,7 +1,11 @@
+'use client'
 import Link from 'next/link'
 import React from 'react'
+import { usePathname } from 'next/navigation'
 
 const Navbar = () => {
+    const pathname = usePathname();
+    console.log('pathname:', pathname)
     return (
         <nav className="flex items-center justify-between px-6 py-4 bg-white border-b border-gray-100">
             <Link href="/" className="flex items-center gap-2">
@@ -12,17 +16,17 @@ const Navbar = () => {
             </Link>
 
             <div className="flex items-center gap-4">
-                <Link href="/" className="flex items-center gap-2 py-2 text-gray-600 transition-colors hover:text-indigo-600 group">
+                <Link href="/" className={`flex items-center gap-2 py-2 transition-colors  px-2 rounded-sm group ${pathname === '/' ? 'bg-teal-700 text-white hover:bg-teal-800 hover:text-white' : 'text-gray-700 hover:bg-teal-100 hover:text-teal-800'}`}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-house-icon lucide-house"><path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" /><path d="M3 10a2 2 0 0 1 .709-1.528l7-6a2 2 0 0 1 2.582 0l7 6A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" /></svg>
                     <span className="hidden sm:inline font-medium">Home</span>
                 </Link>
 
-                <Link href="/timeline" className="flex items-center gap-2  py-2 text-gray-600 transition-colors hover:text-indigo-600 group">
+                <Link href="/timeline" className={`flex items-center gap-2 py-2 transition-colors  px-2 rounded-sm group ${pathname === '/timeline' ? 'bg-teal-700 text-white hover:bg-teal-800 hover:text-white' : 'text-gray-700 hover:bg-teal-100 hover:text-teal-800'}`}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-clock-icon lucide-clock"><circle cx="12" cy="12" r="10" /><path d="M12 6v6l4 2" /></svg>
                     <span className="hidden sm:inline font-medium">Timeline</span>
                 </Link>
 
-                <Link href="/stats" className="flex items-center gap-2 py-2 text-gray-600 transition-colors hover:text-indigo-600 group">
+                <Link href="/stats" className={`flex items-center gap-2 py-2 transition-colors  px-2 rounded-sm group ${pathname === '/stats' ? 'bg-teal-700 text-white hover:bg-teal-800 hover:text-white' : 'text-gray-700 hover:bg-teal-100 hover:text-teal-800'}`}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-chart-no-axes-combined-icon lucide-chart-no-axes-combined"><path d="M12 16v5" /><path d="M16 14v7" /><path d="M20 10v11" /><path d="m22 3-8.646 8.646a.5.5 0 0 1-.708 0L9.354 8.354a.5.5 0 0 0-.707 0L2 15" /><path d="M4 18v3" /><path d="M8 14v7" /></svg>
                     <span className="hidden sm:inline font-medium">Stats</span>
                 </Link>
--- src/app/stats/page.jsx
+++ src/app/stats/page.jsx
@@ -1,11 +1,75 @@
+'use client'
 import React from 'react'
 
-const Stats = () => {
-    return (
-        <div className="autowidth">
-            sdfs
-        </div>
-    )
-}
+import { Pie, PieChart, Cell, Tooltip } from 'recharts';
+import { useData } from '@/context/DataContext';
 
-export default Stats
+export default function PieChartWithPaddingAngle({ isAnimationActive = true }) {
+
+    const { checkIns } = useData();
+    const obj = checkIns.reduce((acc, checkIn) => {
+        const { type } = checkIn;
+        if (!acc[type]) {
+            acc[type] = 0;
+        }
+        acc[type]++;
+        return acc;
+    }, {});
+    const data = Object.keys(obj).map((key) => {
+        return { name: key, value: obj[key] };
+    });
+
+
+
+    const COLORS = ["#8884d8", "#82ca9d", "#ffc658"];
+
+
+
+    return (
+        <>
+            {
+                checkIns.length > 0 ?
+
+                    <div className='flex justify-center items-center flex-col'>
+
+                        <PieChart style={{ width: '100%', maxWidth: '500px', maxHeight: '40vh', aspectRatio: 1 }} responsive>
+                            <Tooltip
+                                contentStyle={{ borderRadius: '8px', border: 'none', boxShadow: '0 4px 6px -1px rgb(0 0 0 / 0.1)' }}
+                                itemStyle={{ color: '#333' }}
+                            />
+                            <Pie
+                                data={data}
+                                innerRadius="80%"
+                                outerRadius="100%"
+                                // Corner radius is the rounded edge of each pie slice
+                                cornerRadius="50%"
+                                // fill="#8884d8"
+
+                                // padding angle is the gap between each pie slice
+                                paddingAngle={5}
+                                dataKey="value"
+                                isAnimationActive={isAnimationActive}
+                            >
+                                {data.map((entry, index) => (
+                                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                                ))}
+                            </Pie>
+                        </PieChart>
+
+                        <div className='flex gap-2'>
+                            {
+                                data.map((d, i) => <p key={i} className='mr-3'><span style={{ backgroundColor: COLORS[i] }} className={`w-4 h-4 inline-block rounded-sm mr-2`}></span>{d.name}</p>)
+                            }
+                        </div>
+
+                    </div>
+                    :
+                    <div className='flex flex-col items-center justify-center h-[40vh] bg-red-100/20 text-red-500'>
+                        <p>NO Check-Ins yet</p>
+                        <p className='text-xl'>Check up on your friedns first</p>
+                    </div>
+            }
+
+        </>
+    );
+}
